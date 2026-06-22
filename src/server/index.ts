3e963c6271d53448925d10cd9306b739eea3e10f
Stuzerain <shanford3@gmail.com>
2026-06-22 17:47:45 -0400

    feat(init): pseudocode endpoints, simple docker-compose file, enumerate types

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,7 +2,43 @@ import express = require('express');
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res) => {
+// operationId: getInventory
+app.get('/inventory/:skuId', (req, res) => {
+    /**
+     * look up skuId
+     * if exists, return 200 + quantity
+     * else return 404
+     */
+  res.send('Hello World!');
+});
+
+// operationId: createInventory
+app.post('/inventory/:skuId', (req, res) => {
+    /**
+     * attempt upsert quantity for skuId
+     * if successful, return 200 + quantity
+     * else return 400
+     */
+  res.send('Hello World!');
+});
+
+// operationId: purchaseitem
+app.post('/inventory/:skuId/purchase', (req, res) => {
+    /**
+     * look up skuId
+     * if exists and purchase amount < quantity, subtract purchase amount
+     *     return 200 + quantity
+     * if insufficient inventory or invalid request, return 400
+     * if SKU not found, return 404
+     */
+  res.send('Hello World!');
+});
+
+// operationId: listInventory
+app.get('/inventory', (req, res) => {
+    /**
+     * returns array of all SKUs + quantity (can be empty)
+     */
   res.send('Hello World!');
 });
 
