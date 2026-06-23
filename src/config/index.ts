e6dffc568c69ff6a6b99d1eb19ceb2e0e7d293ed
Stuzerain <shanford3@gmail.com>
2026-06-23 19:16:18 -0400

    chore(config): a bit more generic config handling

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,11 +1,17 @@
 import { AppConfig } from "../types";
 
+const SERVER_PORT = Number(process.env.SERVER_PORT) || 3000
+const DB_USER = process.env.DB_USER || "postgres"
+const DB_PASS = process.env.DB_PASS || "local_pass"
+const DB_HOST = process.env.DB_HOST || "0.0.0.0"
+const DB_PORT = Number(process.env.DB_PORT) || 5432
+
 export const config: AppConfig = {
-    port: 3000,
+    port: SERVER_PORT,
     dbConfig: {
-        user: "postgres",
-        password: "local_pass",
-        host: "0.0.0.0",
-        port: 5432,
+        user: DB_USER,
+        password: DB_PASS,
+        host: DB_HOST,
+        port: DB_PORT,
     }
 }
