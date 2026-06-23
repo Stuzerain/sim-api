a677e4c9b36ea2e35a3766d695ee94f860454c99
Stuzerain <shanford3@gmail.com>
2026-06-22 23:52:46 -0400

    feat(api): implement API routes w/ DB helper fns

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,47 +1,60 @@
-import express = require('express');
-const app = express();
-const PORT = 3000;
+import express = require('express')
+const app = express()
+app.use(express.json())
+
+import { config } from "../config"
+import { lookupSku, addQuantityToSku, safeSubtractFromSku, getFullInventory } from '../db'
 
 // operationId: getInventory
-app.get('/inventory/:skuId', (req, res) => {
-    /**
-     * look up skuId
-     * if exists, return 200 + quantity
-     * else return 404
-     */
-  res.send('Hello World!');
-});
+app.get('/inventory/:skuId', async (req, res) => {
+    try {
+        const { skuId } = req.params
+        const lookup = await lookupSku(skuId)
+        res.status(200).send(lookup)
+    } catch (error) {
+        res.status(404).send(error)
+    }
+})
 
 // operationId: createInventory
-app.post('/inventory/:skuId', (req, res) => {
-    /**
-     * attempt upsert quantity for skuId
-     * if successful, return 200 + quantity
-     * else return 400
-     */
-  res.send('Hello World!');
-});
+app.post('/inventory/:skuId', async (req, res) => {
+    try {
+        const { quantity } = req.body
+        const { skuId } = req.params
+        const inventoryItem = { skuId, quantity }
+        const upsert = await addQuantityToSku(inventoryItem)
+        res.status(200).send(upsert)
+    } catch (error) {
+        res.status(400).send(error)
+    }
+})
 
 // operationId: purchaseitem
-app.post('/inventory/:skuId/purchase', (req, res) => {
-    /**
-     * look up skuId
-     * if exists and purchase amount < quantity, subtract purchase amount
-     *     return 200 + quantity
-     * if insufficient inventory or invalid request, return 400
-     * if SKU not found, return 404
-     */
-  res.send('Hello World!');
-});
+app.post('/inventory/:skuId/purchase', async (req, res) => {
+    try {
+        const { quantity } = req.body
+        const { skuId } = req.params
+        const inventoryItem = { skuId, quantity }
+        const purchase = await safeSubtractFromSku(inventoryItem)
+        res.status(200).send(purchase)
+    } catch (error) {
+        // default error status is 400, only use 404 when no SKU found
+        let status = 400
+        if (error === "NoDataError: SKU not found") status = 404
+        res.status(status).send(error)
+    }
+})
 
 // operationId: listInventory
-app.get('/inventory', (req, res) => {
-    /**
-     * returns array of all SKUs + quantity (can be empty)
-     */
-  res.send('Hello World!');
-});
+app.get('/inventory', async (req, res) => {
+    try {
+        const inventory = await getFullInventory()
+        res.status(200).send(inventory)
+    } catch (error) {
+        res.status(400).send(error)
+    }
+})
 
-app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Simple Inventory Manager listening on port: ${PORT}`);
-});
+app.listen(config.port, "0.0.0.0", () => {
+  console.log(`Simple Inventory Manager listening on port: ${config.port}`);
+})
